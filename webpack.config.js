ce8ae803b989c3abd5f9978b6849b6e03eca7987
Peter Sharp <peter@webmad.co.nz>
2017-10-30 22:13:59 +1300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+/*
+ * We've enabled ExtractTextPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin
+ *
+ */
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, '.')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['es2015']
+				}
+			},
+			{
+				test: /\.(scss|css)$/,
+
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			}
+		]
+	},
+	devServer: {
+	  contentBase: path.resolve(__dirname, "."),
+	  compress: true,
+	  port: 9000
+	},
+	plugins: [
+		// new UglifyJSPlugin(),
+		new ExtractTextPlugin('bundle.[contentHash].css')
+	]
+};
