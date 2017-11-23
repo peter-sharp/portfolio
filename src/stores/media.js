d11ddfa66f19d59cf4da5651655f7f4f4860592b
Peter Sharp <peter.victor.sharp@gmail.com>
2017-11-23 21:19:17 +1300

    addedMeidaarticles by id

--- src/stores/media.js
+++ src/stores/media.js
@@ -4,11 +4,16 @@ export default function mediaStore(state, events){
   var mediaArticlesAll = []
 
   state.mediaArticles = []
+  state.mediaArticlesById = {}
+
+
+  state.mediaCategories = ['3D','illustration','web-design','animation']
 
   mediaArticleService('list', 'get')
   .then(mediaArticles => {
     mediaArticlesAll = mediaArticles
     state.mediaArticles = mediaArticlesAll;
+    state.mediaArticlesById = mediaArticlesAll.reduce((acc, x) => (acc[x.id] = x), {})
     events.emit(state.events.RENDER);
   })
 
