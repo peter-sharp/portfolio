e9b472b2226f55138910682f3faed2c89817bffc
Peter Sharp <peter.victor.sharp@gmail.com>
2017-11-23 21:13:32 +1300

    fixed media route

--- src/index.js
+++ src/index.js
@@ -9,19 +9,21 @@ import contactView from 'views/contact'
 import notFoundView from 'views/notFound'
 import mediaView from 'views/media'
 import menuStore from 'stores/menu'
+import mainStore from 'stores/main'
 import mediaStore from 'stores/media'
 import pagesStore from 'stores/pages'
 console.info(`%c${briefcase}`, 'color: yellow; text-shadow: 2px 2px 5px rgba(0,0,0,0.5)')
 
 const app = Choo()
 
+app.use(mainStore)
 app.use(menuStore)
 app.use(pagesStore)
 app.use(mediaStore)
 
 app.route('/', mainView.bind(null, homeView))
-app.route('/media/{id}', mainView.bind(null, mediaView))
 app.route('/contact', mainView.bind(null, contactView))
+app.route('/media/:id', mainView.bind(null, mediaView))
 app.route('*', mainView.bind(null, notFoundView))
 
 app.mount('.js-app')
