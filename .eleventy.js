83827c28407a9c300e44efd05bf2a29e22fb77fa
Peter Sharp <peter.victor.sharp@gmail.com>
2019-12-07 00:37:41 +1300

    Makes brosersync https

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = function eleventyConfig(config) {
 
     config.addPassthroughCopy('src/site/uploads')
     config.addPassthroughCopy('src/site/style.css')
-
+    config.setBrowserSyncConfig({
+        https: true
+    });
     return {
         dir: {
             input: 'src/site'
