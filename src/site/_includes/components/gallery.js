e537a4150ba06cb967d8839309df4a8e6f86ea60
Peter Sharp <peter.victor.sharp@gmail.com>
2019-12-23 14:23:26 +1300

    Fixes gallery spacing and item size

--- src/site/_includes/components/gallery.js
+++ src/site/_includes/components/gallery.js
@@ -6,7 +6,7 @@ module.exports = function gallery(items) {
                   ${items.map(({ data, url }) => html`
                   <li class="gallery__item" id="${data.id}">
                       <a href="${url}" >
-                          <img class="image--small" src="/uploads/${data.mediaPaths.small}" alt="${data.title}"/>
+                          <img src="/uploads/${data.mediaPaths.small}" alt="${data.title}"/>
                           <h3>${data.title}</h3>
                       </a>
                   </li>`)}
