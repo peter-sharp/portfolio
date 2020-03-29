af84f14fb619f9994cf1470b7ac3b3840a554254
peter-sharp <Peter@petersharp.co.nz>
2020-03-30 01:02:58 +1300

    Adds image-clip

--- src/site/_includes/components/gallery.js
+++ src/site/_includes/components/gallery.js
@@ -6,9 +6,11 @@ module.exports = function gallery(items) {
                   ${items.map(({ data, url }) => html`
                   <li class="gallery__card card" id="${data.id}">
                       <a href="${url}" class="stack stack--gap-small">
+                         <div class="card__image-clip">
                           ${data.mediaPaths ?
                             html`<img class="card__image" src="/uploads/${data.mediaPaths.small || data.mediaPaths.original}" alt="${data.title}"/>` :
                       html`<div class="card__image-placeholder"></div>`}
+                          </div>
                           <h3 class="card__title" >${data.title}</h3>
                       </a>
                   </li>`)}
