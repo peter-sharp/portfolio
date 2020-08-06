f294b407ee9aa30d83a30b3d39922a884db23b41
Peter Sharp <peter.victor.sharp@gmail.com>
2020-08-06 12:59:07 -0700

    Adds fallback if data.id is missing

--- src/site/_includes/components/gallery.js
+++ src/site/_includes/components/gallery.js
@@ -1,18 +1,22 @@
 const { html } = require('../html.js')
 
 module.exports = function gallery(items) {
-    return html`<ul class="gallery">
-                  
-                  ${items.map(({ data, url }) => html`
-                  <li class="gallery__card card" id="${data.id}">
-                      <a href="${url}" class="stack stack--gap-small">
-                         <div class="card__image-clip">
-                          ${data.mediaPaths ?
-                            html`<img class="card__image" src="/uploads/${data.mediaPaths.thumbnail || data.mediaPaths.original}" alt="${data.title}"/>` :
-                      html`<div class="card__image-placeholder"></div>`}
-                          </div>
-                          <h3 class="card__title" >${data.title}</h3>
-                      </a>
-                  </li>`)}
-              </ul>`
+    return html`
+            <ul class="gallery">
+                  ${items.map(({ data, url }, i) => {
+                      const id = data.id || `gallery_item_${i}`;
+                     return html`
+                    <li class="gallery__card card" id="${id}">
+                        <a href="${url}" class="stack stack--gap-small">
+                            <div class="card__image-clip">
+                            ${data.mediaPaths ?
+                         html`<img class="card__image" src="/uploads/${data.mediaPaths.thumbnail || data.mediaPaths.original}" alt="${data.title}"/>` :
+                        html`<div class="card__image-placeholder"></div>`}
+                            </div>
+                            <h3 class="card__title" >${data.title}</h3>
+                        </a>
+                    </li>`
+                  })
+                  }
+            </ul>`
 }
