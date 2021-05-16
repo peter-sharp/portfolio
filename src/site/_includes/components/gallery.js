bdbf5bee6bdafbe764edc47c8f85159b52cf7483
Peter Sharp <peter.victor.sharp@gmail.com>
2021-05-16 16:11:58 +1200

    Simplifies/improves card styles

--- src/site/_includes/components/gallery.js
+++ src/site/_includes/components/gallery.js
@@ -7,14 +7,12 @@ module.exports = function gallery(items) {
                       const id = data.id || `gallery_item_${i}`;
                      return html`
                     <li class="gallery__card card" id="${id}">
-                        <a href="${url}" class="stack stack--gap-small">
                             <div class="card__image-clip">
                             ${data.mediaPaths ?
                          html`<img class="card__image" src="/uploads/${data.mediaPaths.thumbnail || data.mediaPaths.original}" alt="${data.title}"/>` :
                         html`<div class="card__image-placeholder"></div>`}
                             </div>
-                            <h3 class="card__title" >${data.title}</h3>
-                        </a>
+                            <h3 class="card__title" ><a href="${url}" class="card__link">${data.title}</a></h3>
                     </li>`
                   })
                   }
