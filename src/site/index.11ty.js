df545dd8f7f12ba676aa45fea0b905d8b4b86c9b
Peter Sharp <hello@petersharp.dev>
2022-07-25 22:27:11 -0600

    Moves bio to page top Centers CTA

--- src/site/index.11ty.js
+++ src/site/index.11ty.js
@@ -10,18 +10,19 @@ module.exports.data = {
 
 module.exports.render = function render({ collections }) {
     return html`<div class="stack" data-gap="large">
-        <div>
-            ${gallery(collections.gallery)}
-        </div>
         <article class="h-card section">
-            <figure class="stack stack--gap-medium">
+            <figure class="cluster"  data-state="align-middle">
                 <div class="user-icon-frame">
                     <img class="u-logo image--responsive user-icon-frame__image" src="/uploads/peter-like-a-sir.png"/>
                 </div>
-                <figcaption>I'm <span class="p-name">Peter</span> developer, designer and creative. This my space to show off my creative work.</figcaption>
+                <figcaption>I'm <span class="p-name">Peter</span>: developer, designer and creative.<br> This my space to show off my creative work.</figcaption>
             </figure>
         </article>
-        <div class="cluster section">
+        <div>
+            ${gallery(collections.gallery)}
+        </div>
+        
+        <div class="cluster section center">
             <p>Need help with illustration or design?</p>
             <a href="/contact" class="button" >Hire me</a>
         </div>
